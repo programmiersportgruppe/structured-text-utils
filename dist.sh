f409ced2a57de65798d1f217e2bab940a6856d7f
Felix Leipold <felix.leipold@gmail.com>
2012-09-03 23:39:30 +0200

    adding distribution script

---
+++ dist.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# builds all projects and creates distribution archive
+
+VERSION=0.1
+PLATFORM="`uname`-`uname -m`"
+ARCHIVE_NAME=structured-text-utils-$VERSION-$PLATFORM
+
+cd jsed
+./build.sh
+cd ..
+
+cd stpl
+./build.sh
+cd ..
+
+pwd
+
+mkdir -p $ARCHIVE_NAME/bin
+cp jsed/jsed $ARCHIVE_NAME/bin
+cp stpl/dist/build/stpl/stpl $ARCHIVE_NAME/bin
+
+cp README.md $ARCHIVE_NAME
+
+tar cvfj $ARCHIVE_NAME.tar.bz2 $ARCHIVE_NAME
+
+rm -rf $ARCHIVE_NAME
+
+
+
