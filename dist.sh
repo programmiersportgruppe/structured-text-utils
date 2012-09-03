3fd8597a75d5c6716e0214277cc84299c11b5f50
Felix Leipold <felix.leipold@gmail.com>
2012-09-04 00:33:34 +0200

    adding installer script to distribution tar

--- dist.sh
+++ dist.sh
@@ -2,8 +2,6 @@
 #
 # builds all projects and creates distribution archive
 
-set -e
-
 VERSION=0.1
 PLATFORM="`uname`-`uname -m`"
 ARCHIVE_NAME=structured-text-utils-$VERSION-$PLATFORM
@@ -23,6 +21,17 @@ cp jsed/jsed $ARCHIVE_NAME/bin
 cp stpl/dist/build/stpl/stpl $ARCHIVE_NAME/bin
 
 cp README.md $ARCHIVE_NAME
+
+cat >$ARCHIVE_NAME/install.sh <<'END'
+#!/usr/bin/env bash
+set -e
+echo Installing to /usr/local
+
+mkdir -p /usr/local/bin
+install bin/* /usr/local/bin
+END
+
+chmod a+x $ARCHIVE_NAME/install.sh
 
 tar cvfj $ARCHIVE_NAME.tar.bz2 $ARCHIVE_NAME
 
