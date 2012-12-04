e92287aff259838100f551c5a127cc67928269c6
Felix Leipold <felix.leipold@gmail.com>
2012-12-04 18:14:16 +0100

    [refactoring] extract argument parser and use exceptions

--- jsed/jsed.cpp
+++ jsed/jsed.cpp
@@ -5,10 +5,11 @@
 #include <fstream>
 #include <sstream>
 #include <streambuf>
+#include <exception>
+#include <stdexcept>
 #include "js.cpp"
 #include "jsedjs.h"
 using namespace std;
-
 
 class Transformer {
 
@@ -46,20 +47,77 @@ std::string readFile(const char* filename){
     return buffer.str();
 }
 
+class OptionParser {
+    public:
 
-void usage() {
-    printf("Usage: jsed [options] (transformation | -f file) \n");
-    printf("   transformation   Transformation function\n");
-    printf("   file             File containing transformation function\n");
-    printf("Options:\n");
-    printf("   -m, --multi-docs Expects input to be multiple documents\n");
-    printf("                    formatted on a single line and separated\n");
-    printf("                    by new lines\n");
-    printf("   -r, --raw        Produces raw string output\n");
-    printf("   -p, --pretty     Pretty print json output\n");
-    printf("   -h, --help       Display help message\n");
-    printf("   -d, --debug      Print debug information\n");
-}
+    std::string script;
+    bool showHelp;
+    bool multiple;
+    bool debug;
+    bool raw;
+    bool pretty;
+
+    OptionParser(int argc, const char *argv[]) throw (runtime_error) {
+        for (int i=1; i < argc; i++){
+            string next(argv[i]);
+            if (next == "-m" || next == "--multi-docs") {
+                multiple = true;
+                continue;
+            }
+            if (next == "-h" || next == "--help") {
+                usage();
+                showHelp = true;
+                return;
+            }
+            if (next == "-d" || next == "--debug") {
+                debug = true;
+                continue;
+            }
+            if (next == "-r" || next == "--raw"){
+                raw = true;
+                continue;
+            }
+            if (next == "-p" || next == "--pretty"){
+                pretty = true;
+                continue;
+            }
+            if (next == "-f") {
+                i++;
+                if (i==argc){
+                    throw * new runtime_error("Missing argument value for -f");
+                }
+                script = readFile(argv[i]);
+                continue;
+            }
+            script = next;
+        }
+
+        if (debug) {
+            fprintf(stderr, "Script:   '%s'\n", script.c_str());
+            fprintf(stderr, "Multiple: '%s'\n", multiple ? "true" : "false" );
+            fprintf(stderr, "Raw:      '%s'\n", raw ? "true" : "false" );
+            fprintf(stderr, "Pretty:   '%s'\n", pretty ? "true" : "false" );
+        }
+
+        if (script == "" && !showHelp) {
+            throw  * new runtime_error("Missing transformation function");
+        }
+    }
+
+    void usage() {
+        printf("Usage: jsed [options] (transformation | -f file) \n");
+        printf("   transformation   Transformation function\n");
+        printf("   file             File containing transformation function\n");
+        printf("Options:\n");
+        printf("   -m, --multi-docs Expects input to be multiple documents\n");
+        printf("                    formatted on a single line and separated\n");
+        printf("                    by new lines\n");
+        printf("   -r, --raw        Produces raw string output\n");
+        printf("   -p, --pretty     Pretty print json output\n");
+        printf("   -h, --help       Display help message\n");
+        printf("   -d, --debug      Print debug information\n");
+    }
+};
 
 class Line {
     std::string data;
@@ -73,74 +131,31 @@ class Line {
     }
 };
 
-
-
 int main(int argc, const char *argv[])
 {
-    std::string script = "";
-    bool multiple = false;
-    bool debug = false;
-    bool raw = false;
-    bool pretty = false;
+    try {
+        OptionParser parser(argc, argv);
 
-    for (int i=1; i < argc; i++){
-        string next(argv[i]);
-        if (next == "-m" || next == "--multi-docs") {
-            multiple = true;
-            continue;
-        }
-        if (next == "-h" || next == "--help") {
-            usage();
+        if (parser.showHelp)
             return 0;
-        }
-        if (next == "-d" || next == "--debug") {
-            debug = true;
-            continue;
-        }
-        if (next == "-r" || next == "--raw"){
-            raw = true;
-            continue;
-        }
-        if (next == "-p" || next == "--pretty"){
-            pretty = true;
-            continue;
-        }
-        if (next == "-f") {
-            i++;
-            if (i==argc){
-                fprintf(stderr, "missing argument value for -f");
-                return 1;
-            }
-            script = readFile(argv[i]);
-            continue;
-        }
-        script = next;
-    }
 
-    if (script == "") {
-        fprintf(stderr, "Missing script parameter\n");
-        usage();
+        JSInterpreter js;
+        Function transformation = js.evaluateScript(parser.script);
+        Transformer transformer(js, transformation, parser.raw, parser.pretty);
+
+        if (parser.multiple) {
+            std::transform(istream_iterator<Line>(cin), istream_iterator<Line>(), ostream_iterator<std::string>(cout, "\n"), transformer);
+        } else {
+            std::string input = readStdIn();
+            std::string result = transformer(input);
+            std::cout << result;
+        }
+        std::cout << "\n";
+
+    }
+    catch(runtime_error& ex) {
+        cerr << ex.what() << endl;
         return 1;
     }
 
-    if (debug) {
-        fprintf(stderr, "Script:   '%s'\n", script.c_str());
-        fprintf(stderr, "Multiple: '%s'\n", multiple ? "true" : "false" );
-        fprintf(stderr, "Raw:      '%s'\n", raw ? "true" : "false" );
-        fprintf(stderr, "Pretty:   '%s'\n", pretty ? "true" : "false" );
-    }
-
-    JSInterpreter js;
-    Function transformation = js.evaluateScript(script);
-    Transformer transformer(js, transformation, raw, pretty);
-
-    if (multiple) {
-        std::transform(istream_iterator<Line>(cin), istream_iterator<Line>(), ostream_iterator<std::string>(cout, "\n"), transformer);
-    } else {
-        std::string input = readStdIn();
-        std::string result = transformer(input);
-        std::cout << result;
-    }
-    std::cout << "\n";
-    return 0;
 }
