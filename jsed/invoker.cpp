5330b4d27d0f3f9c3dfa2a335523f6289a0e57d5
Felix Leipold <felix.leipold@gmail.com>
2012-12-23 20:00:14 +0100

    JSED-3 Parameterising command and args

--- jsed/invoker.cpp
+++ jsed/invoker.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <vector>
+
 #include "fdstream.hpp"
 
 using namespace std;
@@ -14,14 +16,12 @@ using namespace std;
  * Filters a string through an external process
  *
  * TODO
- *  - Add timeout functionality (nasty)
- *  - Pass in command
- *  - Pass in arguments (std::list?)
  *  - Deal with non-zero exit code
  *  - Deal with standard-error
+ *  - Add timeout functionality (nasty)
  */
 
-std::string filter(std::string input) {//char* command, char** args, istream input, ostream output){
+std::string filter(std::string input, std::string command, std::vector<std::string> args) {//char* command, char** args, istream input, ostream output){
     int Input[2], Output[2];
 
     pipe( Input );
@@ -37,13 +37,23 @@ std::string filter(std::string input) {//char* command, char** args, istream inp
     }
     else
     {    // We're in the child here.
-         close( Input[ 1 ] );
-         dup2( Input[ 0 ], STDIN_FILENO );
-         close( Output[ 0 ] );
-         dup2( Output[ 1 ], STDOUT_FILENO );
+        close( Input[ 1 ] );
+        dup2( Input[ 0 ], STDIN_FILENO );
+        close( Output[ 0 ] );
+        dup2( Output[ 1 ], STDOUT_FILENO );
 
-         execlp( "grep", "grep", "hello", NULL );
-         exit (0);
+        // The types and casts in the following paragraph
+        // constitute a severe loss of control.
+        char *c_args[args.size() + 2];
+        c_args[0] = (char*)command.c_str();
+
+        for (int i=0; i < args.size() ; i++)
+            c_args[i + 1] = (char*) args[i].c_str();
+
+        c_args[args.size() + 1] = NULL;
+
+        execvp("grep", c_args);
+        exit (0);
     }
 
     boost::fdistream out(Output[0]);
@@ -58,7 +68,10 @@ std::string filter(std::string input) {//char* command, char** args, istream inp
 }
 
 int main(int argc, const char *argv[]){
-    std::string  output = filter("hello world\nwhat is going on\nhello hello?");
+    std::vector<std::string> args;
+    args.push_back("hello");
+
+    std::string output = filter("hello world\nwhat is going on\nhello hello?", "grep", args);
     printf("output:\n%s", output.c_str());
     return 0;
 }
