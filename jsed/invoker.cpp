a22a94f05b674b3c27f24459cc3061eb905b91ec
Felix Leipold <felix.leipold@gmail.com>
2012-12-24 00:01:25 +0100

    Stderr and exit handling
    We do now capture stderr and if the exit code is not 0, we do throw an error with stderr as message.

--- jsed/invoker.cpp
+++ jsed/invoker.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "fdstream.hpp"
+#include <stdexcept>
 
 using namespace std;
 
@@ -16,31 +17,39 @@ using namespace std;
  * Filters a string through an external process
  *
  * TODO
- *  - Deal with non-zero exit code
- *  - Deal with standard-error
+ *  - Return exit code in exception
  *  - Add timeout functionality (nasty)
  */
 
 std::string filter(std::string input, std::string command, std::vector<std::string> args) {//char* command, char** args, istream input, ostream output){
-    int Input[2], Output[2];
+    int Input[2],
+        Output[2],
+        ErrorOutput[2];
+
 
     pipe( Input );
     pipe( Output );
+    pipe( ErrorOutput );
 
-    if( fork() )
+    int pid = fork();
+    if(pid)
     {
         // We're in the parent here.
         // Close the reading end of the input pipe.
-        close( Input[ 0 ] );
+        close(Input[0]);
         // Close the writing end of the output pipe
-        close( Output[ 1 ] );
+        close(Output[1]);
+        close(ErrorOutput[1]);
+
     }
     else
     {    // We're in the child here.
-        close( Input[ 1 ] );
-        dup2( Input[ 0 ], STDIN_FILENO );
-        close( Output[ 0 ] );
-        dup2( Output[ 1 ], STDOUT_FILENO );
+        close(Input[1]);
+        dup2(Input[0], STDIN_FILENO );
+        close(Output[0] );
+        dup2(Output[1], STDOUT_FILENO);
+        close(ErrorOutput[0]);
+        dup2(ErrorOutput[1], STDERR_FILENO);
 
         // The types and casts in the following paragraph
         // constitute a severe loss of control.
@@ -52,26 +61,40 @@ std::string filter(std::string input, std::string command, std::vector<std::stri
 
         c_args[args.size() + 1] = NULL;
 
-        execvp("grep", c_args);
-        exit (0);
+        int retVal = execvp(command.c_str(), c_args);
+        exit (0); //this not needed, after calling execve...
     }
 
     boost::fdistream out(Output[0]);
+    boost::fdistream err(ErrorOutput[0]);
     boost::fdostream in(Input[1]);
 
-    in << input ;
+    in << input;
     close(Input[1]);
 
     std::stringstream buffer;
     buffer << out.rdbuf();
-    return buffer.str();
+
+    std::string output = buffer.str();
+    std::stringstream errBuffer;
+    errBuffer << err.rdbuf();
+    int stat_loc;
+    waitpid(pid, &stat_loc, 0);
+    int exitCode = stat_loc >> 8;
+    if (exitCode != 0)
+        throw * new runtime_error(std::string("Error executing '") + command + "'\n" + errBuffer.str().c_str());
+    return output;
 }
 
 int main(int argc, const char *argv[]){
     std::vector<std::string> args;
-    args.push_back("hello");
 
-    std::string output = filter("hello world\nwhat is going on\nhello hello?", "grep", args);
+    for (int i=2; i<argc;i++){
+
+        args.push_back(argv[i]);
+    }
+
+    std::string output = filter("hello world\nwhat is going on\nhello hello?", argv[1], args);
     printf("output:\n%s", output.c_str());
     return 0;
 }
