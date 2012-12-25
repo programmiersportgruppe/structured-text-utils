9a83a27f4da60bf4291756d75f8946ce3d5324e3
Felix Leipold <felix.leipold@gmail.com>
2012-12-25 16:54:31 +0100

    Hardening build scripts

--- jsed/fetch-dependencies.sh
+++ jsed/fetch-dependencies.sh
@@ -6,6 +6,13 @@ set -x
 mkdir deps
 cd deps
 wget http://ftp.mozilla.org/pub/mozilla.org/js/js185-1.0.0.tar.gz
+status=$?
+if [ $status -ne 0 ]; then
+    echo "Failed to download spidermonkey. wget returned $status." >&2
+    exit 1
+fi
+
+set -e
 tar xzf js185-1.0.0.tar.gz
 
 cd js-1.8.5/js/src
@@ -16,3 +23,4 @@ sudo make install
 
 cd ../../..
 cd ..
+
--- jsed/js/build.sh
+++ jsed/js/build.sh
@@ -5,10 +5,16 @@ rm -f js.o
 STATIC_PREFIX=../deps/js-1.8.5/js/src/dist
 PREFIX=/usr/local
 
+
+
 if [ "$*" = "--static" ]
 then
+    if [ ! -d "$STATIC_PREFIX/include" ]; then
+        echo "Static version of spidermonkey missing. Try running fetch-dependencies.sh" >&2
+        exit 1;
+    fi
     g++ -c js.cpp -I$STATIC_PREFIX/include
 else
-    g++ -I$STATIC_PREFIX/include -c js.cpp -I$PREFIX/include/js
+    g++ -c js.cpp -I$PREFIX/include/js
 fi
 
